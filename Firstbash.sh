0d891c8e5e08445444cead231109b48d38bb8c76
costech2017 <cosmas.nzube@gmail.com>
2023-07-28 13:46:48 +0100

    Welcome in Bash

---
+++ Firstbash.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Read User input
+
+echo "Enter your First Name:"
+
+read first_name
+
+echo "Enter your Last Name:"
+
+read last_name
+
+echo "Enter Username:"
+
+read user_name
+
+echo "Create Password:"
+
+read pass_word
+
+echo
+
+echo "Your Name is:"
+
+echo $first_name, $last_name
+
+echo
+
+echo "Your Username is:"
+
+echo $user_name
+
+echo
+
+echo "Login Password:"
+
+echo $pass_word
+
+echo "Welcome to Poltech Solutions"
